12747955a08363c9e92907fe411fe256fcad4e6f
l0nelyhermit <mrjonathangoh@gmail.com>
2020-08-20 00:10:13 +0800

    added CRUD for accounts into the system

--- app.py
+++ app.py
@@ -30,6 +30,10 @@ class User(flask_login.UserMixin):
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
 # login_manager.login_view = 'login_user'
+login_manager.refresh_view = 'login_user'
+login_manager.needs_refresh_message = (
+    u"To protect your account, please reauthenticate to access this page."
+)
 
 
 @login_manager.user_loader
@@ -57,7 +61,7 @@ all_security_questions = [
     'What was the name of your first stuffed animal'
 ]
 
-
+# Welcome Page of the Website
 @app.route('/')
 def welcome_user():
     return render_template('welcome_user.template.html')
@@ -94,7 +98,7 @@ def register_user():
             username_check = db.Users.find_one({
             'Username': username
             })
-            # Check if user enetered an email that exists in database
+            # Check if user entered an email that exists in database
             email_check = db.Users.find_one({
                 'Email': email
             })
@@ -203,6 +207,7 @@ def register_user():
 
 @app.route('/login', methods=['GET','POST'])
 def login_user():
+    # Retrieves the login webpage for the user to login
     if request.method =="GET":
         return render_template('login_user.template.html',errors={})
     else:
@@ -234,17 +239,152 @@ def login_user():
             return render_template('login_user.template.html')
 
 
-
+# Logout Functionality
 @app.route('/logout')
 def logout():
     flask_login.logout_user()
     flash('Logged Out','success')
     return redirect(url_for('login_user'))
 
+# Main Web Application After Login
 @app.route('/home')
 @flask_login.login_required
 def home():
     return render_template('home.template.html')
+
+
+# Editing User Account Information
+@app.route('/edit/account/',methods=['GET','POST'])
+@flask_login.login_required
+def edit_user_account():
+    # Retrieves the edit account page for the user to edit account particulars
+    if request.method =="GET":
+        current_user = db.Users.find_one({
+            'Email':flask_login.current_user.get_id()
+        })
+        return render_template('update_account.template.html',current_user=current_user,questions=all_security_questions)
+    else:
+        # Retrieve the updated fields information
+        fname = request.form.get('name')
+        email = request.form.get('email')
+        dob = request.form.get('dob')
+        country = request.form.get('country')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        securityquestion = request.form.get('SecurityQuestion')
+        securityanswer = request.form.get('SecurityAnswer')
+    
+
+        # Form Field Validation
+        # Error Accumulator Collector
+        errors = {}
+        
+        database_error = False
+        # Check if Full Name entered is valid
+        if len(fname) <3 or len(fname)>70:
+            flash('Error: Invalid Name', 'danger')
+            errors.update(invalid_name="Please key in a valid name")
+        
+        # Database Validation
+        elif database_error == False:
+            # Check if the User already exists in the database
+            username_check = db.Users.find_one({
+            'Username': username
+            })
+            # Check if user entered an email that exists in database
+            email_check = db.Users.find_one({
+                'Email': email
+            })
+            # SCENARIOS:
+            # Check against database for existing information that is the same with other users
+            # 1. User wishes to keep email, change username
+            # 2. User wishes to keep username , change email
+            if not username_check:
+                if email_check:
+                    # If user enters same email as another existing user, raise error
+                    if email == email_check['Email']:
+                        flash('Error: Email has already been used, please try again with another valid email','danger')
+                        errors.update(invalid_email="Email has already been used, please enter a valid email")
+                        database_error = True
+                else:
+                    # Check if errors occurred, if there are errors, display failure message
+                    if len(errors) > 0:
+                        flash("Update Failure",'danger')
+                        return redirect(url_for('edit_user_account'))
+                    else:
+
+                        db.Profiles.update({
+                            'Email': flask_login.current_user.get_id()
+                        }, {
+                            '$set': {
+                                'Full_Name': fname,
+                                'Email': email,
+                                'Date_Of_Birth': datetime.datetime.strptime(dob, "%Y-%m-%d"),
+                                'Country': country,
+                                'Username': username,
+                            }
+                        })
+
+                        db.Users.update({
+                            'Email': flask_login.current_user.get_id()
+                        }, {
+                            '$set': {
+                                    'Full_Name': fname,
+                                    'Email': email,
+                                    'Date_Of_Birth': datetime.datetime.strptime(dob, "%Y-%m-%d"),
+                                    'Country': country,
+                                    'Username': username,
+                                    'Password': pbkdf2_sha256.hash(password),
+                                    'SecurityQuestion': securityquestion,
+                                    'SecurityAnswer': securityanswer
+                            }
+                        })
+
+                        flash("Your Account has been updated successfully,Please Login Again","success")
+
+                        return redirect(url_for('welcome_user'))
+            else:
+                 #  If user enters username that matches another existing username, raise error
+                if username == username_check['Username']:
+                    flash('Error: account already exists, please try again with another valid username','danger')
+                    errors.update(invalid_username="Account already exists,please proceed to login")
+                    database_error = True
+                    # if user enters username and email that matches another existing username and email, raise error
+                    if email_check:
+                        if email_check['Email'] ==email and username_check['Username']==username:
+                            flash('Error: Email and Username have been used already,please enter something different','danger')
+                            errors.update(invalid_email_and_username="Email and Username have been used, please try something different")
+                            database_error = True
+
+            if len(errors) > 0:
+                        flash("Update Failure",'danger')
+                        return redirect(url_for('edit_user_account'))
+
+
+
+# Delete Account from Database
+@app.route('/delete/account/',methods=['GET','POST'])
+@flask_login.login_required
+def delete_user_account():
+    # Retrieve the information of the user from the database
+    if request.method =="GET":
+        current_user = db.Users.find_one({
+            'Email':flask_login.current_user.get_id()
+        })
+        return render_template('delete_account.template.html',current_user=current_user)
+    else:
+        # Remove both profile and account from the database
+        db.Profiles.remove({
+            'Email':flask_login.current_user.get_id()
+        })
+        db.Users.remove({
+            'Email':flask_login.current_user.get_id()
+        })
+        flash("Your Account has been deleted successfully","success")
+        return redirect(url_for('welcome_user'))
+
+
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
