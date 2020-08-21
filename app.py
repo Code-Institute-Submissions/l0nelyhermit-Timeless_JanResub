fe20c18509ec85a7c45aa1cde944ebe32b50fbc1
l0nelyhermit <mrjonathangoh@gmail.com>
2020-08-21 11:47:07 +0800

    created basic UI/UX for welcome screen and created functionality for editing profile and displaying profile

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import random
 # Load Environment Variables
 load_dotenv()
 
-app= Flask(__name__)
+app= Flask(__name__, static_url_path='/static')
 
 # Setting up the secret key to the Flask app
 app.secret_key = os.environ.get('SECRET_KEY')
@@ -45,6 +45,7 @@ def user_loader(email):
     if user:
         user_object = User()
         user_object.id = user["Email"]
+        user_object.username = user["Username"]
         return user_object
     else:
         # If the user is not found in the database, raise an error
@@ -69,11 +70,10 @@ def welcome_user():
 # Account creation system (Process Registration)
 @app.route('/register',methods=['GET','POST'])
 def register_user():
-    # Retrieves the Form for the User to  Register
     if request.method =="GET":
         return render_template('register_user.template.html', questions=all_security_questions, errors={})
     else:
-    # Retrieve the information from the registration form
+        # Retrieve the information from the registration form
         fname = request.form.get('name')
         email = request.form.get('email')
         dob = request.form.get('dob')
@@ -82,8 +82,8 @@ def register_user():
         password = request.form.get('password')
         securityquestion = request.form.get('SecurityQuestion')
         securityanswer = request.form.get('SecurityAnswer')
-    
-    # Form Field Validation
+        
+        # Form Field Validation
         # Error Accumulator Collector
         errors = {}
         
@@ -92,7 +92,7 @@ def register_user():
         if len(fname) <3 or len(fname)>70:
             flash('Error: Invalid Name', 'danger')
             errors.update(invalid_name="Please key in a valid name")
-    # Database Validation
+        # Database Validation
         # Check if the User already exists in the database
         elif database_error == False:
             username_check = db.Users.find_one({
@@ -100,7 +100,7 @@ def register_user():
             })
             # Check if user entered an email that exists in database
             email_check = db.Users.find_one({
-                'Email': email
+                    'Email': email
             })
             # SCENARIOS:
             # 1. If user enters same username as another exisitng user, raise error
@@ -116,101 +116,98 @@ def register_user():
                         flash('Error: Email has already been used, please try again with another valid email','danger')
                         errors.update(invalid_email="Email has already been used, please enter a valid email")
                         database_error = True
-                else:
-                    if len(errors) > 0:
-                        flash("Registration Failure",'danger')
-                        return render_template('register_user.template.html',questions=all_security_questions,
-                                                    errors=errors)
                     else:
-                        #  No errors in registration, proceed with registration
-                        # Create a new account and new profile for the user
-                    
-                        # Instance Variable for the Maximum Number Of Users for the site
-                        max_capacity_users = 9999
-                        # Issues a random ID number for the account ID
-                        number = random.randint(0, max_capacity_users)
+                        if len(errors) > 0:
+                            flash("Registration Failure",'danger')
+                            return render_template('register_user.template.html',questions=all_security_questions,
+                                                        errors=errors)
+                        else:
+                            #  No errors in registration, proceed with registration
+                            # Create a new account and new profile for the user
+                        
+                            # Instance Variable for the Maximum Number Of Users for the site
+                            max_capacity_users = 9999
+                            # Issues a random ID number for the account ID
+                            number = random.randint(0, max_capacity_users)
 
-                        # Issues a random ID number for the profile ID
-                        profileID = random.randint(0,max_capacity_users)
+                            # Issues a random ID number for the profile ID
+                            profileID = random.randint(0,max_capacity_users)
 
-                        new_user = {
-                            'AccountID': number,
-                            'Full_Name': fname,
-                            'Email': email,
-                            'Date_Of_Birth': datetime.datetime.strptime(dob, "%Y-%m-%d"),
-                            'Country': country,
-                            'Username': username,
-                            'Password': pbkdf2_sha256.hash(password),
-                            'SecurityQuestion': securityquestion,
-                            'SecurityAnswer': securityanswer
-                        }
-
-                        # Using Flags Technique, if account can be successfully created, proceed with profile creation
-
-                        create_account_success = False
-                        try:
-                            db.Users.insert_one(new_user)
-                            create_account_success = True
-                        except Exception as e:
-                            return str(e)
-
-                        if create_account_success == True:
-                            # extra variables for profile
-                            bio = ''
-                            datejoined = datetime.datetime.now()
-                            new_profile = {
-                                'ProfileID':profileID,
-                                'AccountID': new_user,
+                            new_user = {
+                                'AccountID': number,
                                 'Full_Name': fname,
                                 'Email': email,
                                 'Date_Of_Birth': datetime.datetime.strptime(dob, "%Y-%m-%d"),
                                 'Country': country,
                                 'Username': username,
-                                'Bio': bio,
-                                'Date_Joined': datejoined
+                                'Password': pbkdf2_sha256.hash(password),
+                                'SecurityQuestion': securityquestion,
+                                'SecurityAnswer': securityanswer
                             }
 
-                            db.Profiles.insert_one(new_profile)
+                            # Using Flags Technique, if account can be successfully created, proceed with profile creation
 
-                            flash("Your Account has been created", "success")
-                            user_logged_in = User()
-                            user_email = db.Users.find_one({
-                                'Email':email
-                            })
-                            user_logged_in.id = user_email['Email']
-                            flask_login.login_user(user_logged_in)
+                            create_account_success = False
+                            try:
+                                db.Users.insert_one(new_user)
+                                create_account_success = True
+                            except Exception as e:
+                                return str(e)
 
-                            return redirect(url_for('home'))
+                            if create_account_success == True:
+                                # extra variables for profile
+                                bio = ''
+                                datejoined = datetime.datetime.now()
+                                new_profile = {
+                                    'ProfileID':profileID,
+                                    'AccountID': new_user,
+                                    'Full_Name': fname,
+                                    'Email': email,
+                                    'Date_Of_Birth': datetime.datetime.strptime(dob, "%Y-%m-%d"),
+                                    'Country': country,
+                                    'Username': username,
+                                    'Bio': bio,
+                                    'Date_Joined': datejoined
+                                }
 
-            else:
-                 #  If user enters username that matches another existing username, raise error
-                if username == username_check['Username']:
-                    flash('Error: account already exists, please try again with another valid username','danger')
-                    errors.update(invalid_username="Account already exists,please proceed to login")
-                    database_error = True
-                    # if user enters username and email that matches another existing username and email, raise error
-                    if email_check:
-                        if email_check['Email'] ==email and username_check['Username']==username:
-                            flash('Error: Email and Username have been used already,please enter something different','danger')
-                            errors.update(invalid_email_and_username="Email and Username have been used, please try something different")
-                            database_error = True
-           
+                                db.Profiles.insert_one(new_profile)
 
-            if len(errors) > 0:
-                        flash("Registration Failure",'danger')
-                        return render_template('register_user.template.html',questions=all_security_questions,
-                                                    errors=errors)
+                                flash("Your Account has been created", "success")
+                                user_logged_in = User()
+                                user_data = db.Users.find_one({
+                                    'Email':email
+                                })
+                                user_logged_in.id = user_data['Email']
+                                user_logged_in.username = user_data['Username']
+                                flask_login.login_user(user_logged_in)
 
-    
+                                return redirect(url_for('home'))
+
+                else:
+                    #  If user enters username that matches another existing username, raise error
+                    if username == username_check['Username']:
+                        flash('Error: account already exists, please try again with another valid username','danger')
+                        errors.update(invalid_username="Account already exists,please proceed to login")
+                        database_error = True
+                        # if user enters username and email that matches another existing username and email, raise error
+                        if email_check:
+                            if email_check['Email'] ==email and username_check['Username']==username:
+                                flash('Error: Email and Username have been used already,please enter something different','danger')
+                                errors.update(invalid_email_and_username="Email and Username have been used, please try something different")
+                                database_error = True
+            
+
+                if len(errors) > 0:
+                            flash("Registration Failure",'danger')
+                            return render_template('register_user.template.html',questions=all_security_questions,
+                                                        errors=errors)
+
+        
 
 
 
-@app.route('/login', methods=['GET','POST'])
+@app.route('/', methods=['POST'])
 def login_user():
-    # Retrieves the login webpage for the user to login
-    if request.method =="GET":
-        return render_template('login_user.template.html',errors={})
-    else:
     # Retrieve the information from the login form
         email = request.form.get('email')
         password = request.form.get('password')
@@ -225,6 +222,7 @@ def login_user():
         if check_user and pbkdf2_sha256.verify(password,check_user['Password']):
             user_logged_in = User()
             user_logged_in.id = check_user['Email']
+            user_logged_in.username = check_user['Username']
             flask_login.login_user(user_logged_in)
 
             flash('Login successful','success')
@@ -232,11 +230,11 @@ def login_user():
         # If the user does not exist, raise error
         elif not check_user:
             flash('Invalid Login, no account exists','danger')
-            return render_template('login_user.template.html')
+            return redirect(url_for('welcome_user'))
         # If the login failed due to incorrect input, raise error
         else:
             flash('Wrong Email or Password','danger')
-            return render_template('login_user.template.html')
+            return redirect(url_for('welcome_user'))
 
 
 # Logout Functionality
@@ -244,7 +242,7 @@ def login_user():
 def logout():
     flask_login.logout_user()
     flash('Logged Out','success')
-    return redirect(url_for('login_user'))
+    return redirect(url_for('welcome_user'))
 
 # Main Web Application After Login
 @app.route('/home')
@@ -259,10 +257,10 @@ def home():
 def edit_user_account():
     # Retrieves the edit account page for the user to edit account particulars
     if request.method =="GET":
-        current_user = db.Users.find_one({
+        current_user_logged_in = db.Users.find_one({
             'Email':flask_login.current_user.get_id()
         })
-        return render_template('update_account.template.html',current_user=current_user,questions=all_security_questions)
+        return render_template('update_account.template.html',current_user_logged_in=current_user_logged_in,questions=all_security_questions)
     else:
         # Retrieve the updated fields information
         fname = request.form.get('name')
@@ -383,6 +381,56 @@ def delete_user_account():
         flash("Your Account has been deleted successfully","success")
         return redirect(url_for('welcome_user'))
 
+# Go To Profile Page of the User
+@app.route('/profile',methods=['GET'])
+@flask_login.login_required
+def show_user_profile():
+    current_profile = db.Profiles.find_one({
+        'Email':flask_login.current_user.get_id()
+    })
+    return render_template('show_profile.template.html',current_profile=current_profile)
+
+
+# Allowing User to Edit Profile
+@app.route('/edit/profile',methods=['GET','POST'])
+def edit_user_profile():
+    if request.method == 'GET':
+        # Retrieving the information of the user profile from the database
+        current_profile = db.Profiles.find_one({
+            'Email': flask_login.current_user.get_id()
+        })
+        return render_template('update_profile.template.html',current_profile=current_profile)
+    else:
+        # Retrieve the updated fields information
+        bio = request.form.get('bio')
+        fname = request.form.get('name')
+        country = request.form.get('country')
+
+        # Field Validation
+        # Error Accumulator
+        errors={}
+
+        # Check if input name is valid
+        if len(fname) <3 or len(fname)>70:
+            flash('Error: Invalid Name', 'danger')
+            errors.update(invalid_name="Please key in a valid name")
+            # If errors discovered, raise error and redirect to edit user profile form
+            if len(errors) > 0:
+                flash("Update Failure",'danger')
+                return redirect(url_for('edit_user_profile'))
+        else:
+            db.Profiles.update({
+                'Email': flask_login.current_user.get_id()
+                }, {
+                    '$set': {
+                        'Full_Name': fname,
+                        'Bio': bio,
+                        'Country': country,
+                            }
+                        })
+            
+            flash("Your Profile has been updated successfully","success")
+            return redirect(url_for('show_user_profile'))
 
 
 
