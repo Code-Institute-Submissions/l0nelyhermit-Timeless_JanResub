2dfe504f5bb57672729644c56f548a621fd1d914
l0nelyhermit <mrjonathangoh@gmail.com>
2020-08-24 23:04:47 +0800

    added ability for user to edit their own post and delete their own post. Removed test.py as well

--- test.py
+++
@@ -1,8 +0,0 @@
-import random
-
-max_capacity_users = 9999
-# Issues a random ID number for the account ID
-account_number_list = list(range(1,max_capacity_users))
-random.shuffle(account_number_list)
-number = account_number_list.pop()
-print(number)
