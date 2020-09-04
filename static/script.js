d0a05d5fa75f78c529f0013ade07535a236e07d4
l0nelyhermit <mrjonathangoh@gmail.com>
2020-09-04 10:26:53 +0800

    removed uneccessary javascript code

--- static/script.js
+++ static/script.js
@@ -1,3 +1,4 @@
+// Function for Upvoting a Post
 function upvote(a){
     let votes = parseInt($(a).parent().children('div').text())
     let post_id =$(a).data('postid')
@@ -12,7 +13,7 @@ function upvote(a){
                               'Votes': votes})
     });
 }
-
+// Function for Downvoting a Post
 function downvote(a){
     let votes = parseInt($(a).parent().parent('div').text())
     votes -=1;
@@ -32,7 +33,7 @@ function downvote(a){
     })
     return false;
 }
-
+// Function for liking of Listing
 function like(a){
     let likes = parseInt($(a).parent().children('div').text())
     let like_id =$(a).data('listingid')
@@ -48,6 +49,7 @@ function like(a){
     });
 }
 
+// Single Page Application Functionality for Login Page and Welcome Page
 function hideAllPages() {
     let pages = $(".page");
     for (let p of pages) {
@@ -63,19 +65,6 @@ $('#page2-btn').click(function(){
     $(`#page-${pageNumber}`).removeClass('hidden');
 })
 
-$('#page3-btn').click(function(){
-    let pageNumber = $(this).data('page');
-    hideAllPages();
-    $(`#page-${pageNumber}`).addClass('show');
-    $(`#page-${pageNumber}`).removeClass('hidden');
-})
-
-$('#page4-btn').click(function(){
-    let pageNumber = $(this).data('page');
-    hideAllPages();
-    $(`#page-${pageNumber}`).addClass('show');
-    $(`#page-${pageNumber}`).removeClass('hidden');
-})
 
 // Main Forum Filters
 $('#filter-text-recent').click(function(event){
@@ -98,15 +87,14 @@ $('#filter-text-likes').click(function(event){
     window.location='/marketplace?sort_on=Likes'
 })
 
-// Functionality for the two radio buttons on the forms
+
 
 
 
 
 $(function(){
-    // Implementing functionality for Login Function
 
-
+    // Summernote Functionality
     $('#summernote').summernote({
         placeholder: 'Write Something!',
         tabsize: 2,
@@ -127,12 +115,13 @@ $(function(){
         ]
     });
 
+    // Used to Toggle the user sidebar
     $('#sidebarCollapse').on('click',function(){
         $('#sidebar,#content').toggleClass('active')
     })
 
 
-
+// Function used to toggle the different forms to display for Global Search
      
     $('#listing-radio').on("change",function(){
         $('#listing-search').show();
